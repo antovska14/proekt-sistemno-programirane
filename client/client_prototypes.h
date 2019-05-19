b72407c1aceaa167efcf1278775e6e3eb2d1d53b
Dijana Antovska <antovska14@gmail.com>
2019-05-19 21:22:51 +0300

    Project folders rearanged

--- client/client_prototypes.h
+++ client/client_prototypes.h
@@ -1,12 +1,17 @@
+//add_route_implemetation.c
 Route initializeRoute();
 void addNewRoute(int socket_fd);
 
+//delete_route_implementation.c
 void deleteRoute(int socket_fd);
 
+//fastest_route_print.c
 void printFastestRoute(int socket_fd);
 
+//shortest_route_print.c
 void printShortestRoute(int socket_fd);
 
+//client_ctp_socket_implementation.c
 void printMenu();
 int createSocket();
 void connectToServer(int socket_fd);
--- server/server_prototypes.h
+++ server/server_prototypes.h
@@ -1,20 +1,26 @@
+//database
+//database_conection.c
 void connectToDatabase();
-
+//load_routes.c
 void loadRoutesFromDatabase();
 
+//service
+//add_route.c
 void addRouteToDatabase(Route newRoute);
 int compareRoutes(Route route1, Route route2);
 void addRouteToRoutesList(Route newRoute);
 void addNewRoute(int client_connection_fd);
-
+//delete_route.c
 void deleteRouteFromDatabase(char* startDestination, char* endDestination);
 void deleteRouteFromRoutesList(char* startDestination, char* endDestination);
 void deleteRoute();
-
+//fastest_route.c
 void getFastestRoute(int client_connection_fd);
-
+//shortest_route.c
 void getShortestRoute(int client_connection_fd);
 
+//comunication
+//server_tcp_socket_implementation.c
 int connectToClient(int socket_fd);
 void makeServerListen(int socket_fd);
 struct sockaddr_in initializeServer();
